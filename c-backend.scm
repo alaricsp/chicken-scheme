ab000392bc1887db2a5c0c3aab0ec8bf2ada51a8
Felix Winkelmann <bunny351@gmail.com>
2007-12-27 23:09:38 +0000

    resolved conflict
    git-svn-id: https://galinha.ucpel.tche.br/svn/chicken-eggs/chicken/trunk@7216 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

--- c-backend.scm
+++ c-backend.scm
@@ -1094,7 +1094,7 @@
 	   (gen #t "/* from " (cleanup rname) " */") )
 	 (generate-foreign-callback-header "" stub)
 	 (gen #\{ #t "C_word x,s=" sizestr ",*a=C_alloc(s);")
-	 (gen #t "C_callback_adjust_stack(a,s);")
+	 (gen #t "C_callback_adjust_stack(a,s);") ; make sure content is below stack_bottom as well
 	 (for-each
 	  (lambda (v t)
 	    (gen #t "x=" (foreign-result-conversion t "a") v ");"
