cbda9b61657461722edc2b2f440cf7671f7cfda8
Elf <elf@ephemeral.net>
2008-01-23 13:27:06 +0000

    added special reader tokens: ##current-line and ##current-file i make no claims as to the efficiency.  they should probably be moved into the following clause to avoid penalising most lookups.
    git-svn-id: https://galinha.ucpel.tche.br/svn/chicken-eggs/chicken/trunk@7876 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

--- library.scm
+++ library.scm
@@ -2377,6 +2377,11 @@ EOF
 		   [toklen (##sys#size tok)] )
 	      (unless ##sys#enable-qualifiers 
 		(##sys#read-error port "qualified symbol syntax is not allowed" tok) )
+              (cond ((equal? "current-line" tok)
+                        (##sys#slot port 4))
+                    ((equal? "current-file" tok)
+                        (port-name port))
+              (else
 	      (let loop ([i 0])
 		(cond [(fx>= i toklen) (##sys#read-error port "invalid qualified symbol syntax" tok)]
 		      [(fx= (##sys#byte tok i) (char->integer #\#))
@@ -2385,7 +2390,7 @@ EOF
 		       (##sys#setbyte p 0 i)
 		       (##sys#intern-symbol
 			(string-append p (##sys#substring tok 0 i) (##sys#substring tok (fx+ i 1) toklen)) ) ]
-		      [else (loop (fx+ i 1))] ) ) ) )
+		      [else (loop (fx+ i 1))] ) ) ) ) ) )
 
 	  (define (resolve-symbol tok)
 	    (let ([len (##sys#size tok)])
