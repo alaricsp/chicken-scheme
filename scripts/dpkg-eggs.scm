53cbe6b6e7d5965d2faa27691b24a96e91a841a8
Ivan Raikov <ivan.g.raikov@gmail.com>
2008-02-11 09:45:45 +0000

    Added dpkg-eggs script.
    git-svn-id: https://galinha.ucpel.tche.br/svn/chicken-eggs/chicken/trunk@8366 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

---
+++ scripts/dpkg-eggs.scm
@@ -0,0 +1,120 @@
+;;
+;; Given a directory tree with egg directories, build Debian packages
+;; for all eggs that have a debian subdirectory.
+;;
+;; Usage: dpkg-eggs --eggdir=DIR --output-dir=DIR
+;;
+
+(require-extension srfi-13)
+(require-extension posix)
+(require-extension regex)
+(require-extension utils)
+(require-extension args)
+
+(include "tools.scm")
+
+(define s+ string-append)
+
+(define opts
+  `(
+    ,(args:make-option (eggdir)       (required: "DIR")    
+		       (s+ "operate on eggs in directory DIR"))
+    ,(args:make-option (output-dir)       (required: "DIR")    
+		       (s+ "place Debian packages in directory DIR (will be created if it does not exist)"))
+    ,(args:make-option (verbose)       #:none
+		       (s+ "enable verbose mode")
+		       (set! *verbose* #t))
+    ,(args:make-option (h help)  #:none               "Print help"
+		       (usage))
+
+    ))
+
+
+;; Use args:usage to generate a formatted list of options (from OPTS),
+;; suitable for embedding into help text.
+(define (usage)
+  (print "Usage: " (car (argv)) " options... [list of eggs to be built] ")
+  (newline)
+  (print "The following options are recognized: ")
+  (newline)
+  (print (parameterize ((args:indent 5)) (args:usage opts)))
+  (exit 1))
+
+
+;; Process arguments and collate options and arguments into OPTIONS
+;; alist, and operands (filenames) into OPERANDS.  You can handle
+;; options as they are processed, or afterwards.
+(define args    (command-line-arguments))
+(set!-values (options operands)  (args:parse args opts))
+
+(define dirsep (string ##sys#pathname-directory-separator))
+
+(define (read-subdirs path)
+  (find-files path directory? cons (list) 0))
+
+;; Compare versions of the format x.x...
+(define (version< v1 v2)
+  (let ((v1 (string-split v1 "."))
+	(v2 (string-split v2 ".")))
+    (every (lambda (s1 s2) 
+	     (let ((n1 (string->number s1))
+		   (n2 (string->number s2)))
+	       (cond ((and n1 n2)  (<= n1 n2))
+		     (else (string<= s1 s2)))))
+	   v1 v2)))
+	    
+;; Find the latest release in a given egg directory 
+(define (find-latest-release path)
+  (let ((tags (s+ path dirsep "tags")))
+    (cond ((file-exists? tags) 
+	   (let ((lst (read-subdirs tags)))
+	     (if (pair? lst) (car (sort lst version<))
+		 (error 'find-latest-tag "no releases found in " tags))))
+	  (else path))))
+	    
+;; Find the debian subdirectory in a given egg directory
+(define (find-debian-subdir path . rest)
+  (let-optionals rest ((release (find-latest-release path)))
+    (cond ((file-exists? (s+ release dirsep "debian")) => identity)
+	  ((file-exists? (s+ path dirsep "trunk" dirsep "debian")) => identity)
+	  (else #f))))
+
+(define (build-deb eggdir output-dir path)
+  (let* ((name     (pathname-file path))
+	 (release  (find-latest-release path))
+	 (debdir   (find-debian-subdir path release)))
+    (if debdir
+	(let ((start (cwd)))
+	  (message "Release directory is ~a" release)
+	  (message "debian subdirectory found in ~a" path)
+	  (run (rm -rf ,(s+ output-dir dirsep name)))
+	  (run (cp -R ,release ,(s+ output-dir dirsep name)))
+	  (run (cp -R ,debdir ,(s+ output-dir dirsep name)))
+	  (cd  (s+ output-dir dirsep name))
+	  (run (chmod a+rx debian/rules))
+	  (run (,(s+ "EGG_TREE=\"" eggdir "\"") dpkg-buildpackage -us -uc)))
+	(message "No debian subdirectory found in ~a" path))))
+
+(define (main options operands)
+  (let ((opt_eggdir (alist-ref 'eggdir options))
+	(opt_output-dir (alist-ref 'output-dir options)))
+    (if (not (and opt_eggdir opt_output-dir))
+	(begin
+	  (error-message "Both egg directory and output directory must be specified!")
+	  (usage)))
+    (message "Egg directory tree: ~a" opt_eggdir)
+    (message "Output directory tree: ~a" opt_output-dir)
+    ;; make sure target dir exists
+    (if (not (file-exists? opt_output-dir))
+	(begin
+	  (message "Creating directory ~a" opt_output-dir)
+	  (create-directory opt_output-dir)))
+    (let ((eggdirs (or (and (pair? operands) (map (lambda (x) (s+ opt_eggdir dirsep (->string x))) operands))
+		       (read-subdirs opt_eggdir))))
+      (if (null? eggdirs)
+	  (message "No egg directories found in ~a" opt_eggdir)
+	  (message "Found egg directories: ~a" eggdirs))
+      (for-each (lambda (x) (build-deb opt_eggdir opt_output-dir x))
+		eggdirs))))
+
+(main options operands)
