b7383d7bc1ce59637974c82445d587ba0759fd15
Felix Winkelmann <bunny351@gmail.com>
2008-04-11 13:51:45 +0000

    this is hard
    git-svn-id: https://galinha.ucpel.tche.br/svn/chicken-eggs/chicken/branches/beyond-hope@10423 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

--- mtest.scm
+++ mtest.scm
@@ -5,10 +5,11 @@
  ()
  (a b c)
  values
- (define (a x) x)
+ (define (a x y) (##sys#cons x y))
+ (define d 100)
  (define-syntax c
    (syntax-rules ()
-     ((_ x) (a x))))
+     ((_ x) (a d x))))
  (define b (c 3)))
 
 (print "bar")
@@ -18,6 +19,6 @@
  ((foo values))
  ()
  values
- (define xxx (a 99))
+ (define xxx (a b 99))
  (##sys#print xxx #t ##sys#standard-output)
  (##sys#print (c xxx) #t ##sys#standard-output))
