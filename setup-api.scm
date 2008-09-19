4aaf3aec53620c4876a3d1814556fb5330ab21f5
Felix Winkelmann <bunny351@gmail.com>
2008-09-19 22:20:50 +0000

    removed last use of quotewrap
    git-svn-id: https://galinha.ucpel.tche.br/svn/chicken-eggs/chicken/branches/hygienic@11974 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

--- setup-api.scm
+++ setup-api.scm
@@ -583,7 +583,7 @@
 	   (let ((destf (make-pathname *doc-path* f)))
 	     (copy-file f destf #f)
 	     (unless *windows-shell*
-	       (run (chmod a+rx ,(quotewrap destf))) ) ))
+	       (run (chmod a+rx ,destf)) ) ))
 	 (cdr exs))
 	(newline))
       (write-info id dests info) ) ) )
