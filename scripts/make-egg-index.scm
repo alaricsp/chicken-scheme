594a813f82f204f3ed7a2ecc9f9b97fc8b707f8c
Felix Winkelmann <bunny351@gmail.com>
2009-03-18 09:21:57 +0000

    make-egg-index usable now
    git-svn-id: https://galinha.ucpel.tche.br/svn/chicken-eggs/chicken/trunk@13810 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

--- scripts/make-egg-index.scm
+++ scripts/make-egg-index.scm
@@ -2,11 +2,63 @@
 
 (load-relative "tools.scm")
 
-(use setup-download matchable htmlprag data-structures)
+(use setup-download matchable htmlprag data-structures regex)
+
+(import irregex)
 
 (define *major-version* (##sys#fudge 41))
 
-(define +stylesheet+ "")
+(define +link-regexp+
+  '(: #\[ #\[ (submatch (* (~ #\] #\|))) #\] #\]))
+
+(define +stylesheet+ #<<EOF
+/* table mods by zb */
+table {
+  background: #f6f6ff;
+  padding: 0.2em;
+  margin: 1.2em 2.0em;
+  border: 1px solid #aac;
+  border-collapse: collapse;
+  font-size: 100%;
+}
+th {
+  text-align: left;
+  border-bottom: 1px solid #aac;
+  border-left: 1px solid #aac; 
+  padding: 0.25em 1.0em 0.25em 1.0em;
+} 
+td { 
+  padding: 0.25em 1.0em 0.25em 1.0em; 
+  border-left: 1px solid #aac; 
+}
+blockquote, pre {
+  background-color: #fafaff;
+  display: block;
+  border: 1px dashed gray;
+  margin: 1.0em 0em;
+  padding: 0.5em 1.0em;
+  overflow: auto;
+}
+pre {
+  line-height: 1.3;
+}
+h2, h3, h4, h5, h6 {
+   color: #226;
+   padding-top: 1em;
+}
+
+h1 {
+    background-color: #336;
+	color: #aab;
+	width: 100%;
+	padding: 0;
+    padding: 0.25em 16px 0.25em 0.5em;
+	margin: 0 0 0em 0;
+	font-size: 160%;
+}
+
+EOF
+)
 
 (define +categories+
   '((lang-exts "Language extensions")
@@ -46,10 +98,7 @@
   (exit code))
 
 (define (make-egg-index dir)
-  (let ((title 
-	 (sprintf "Eggs Unlimited (release branch ~a, updated ~a)"
-		  *major-version*
-		  (string-chomp (seconds->string (current-seconds)))))
+  (let ((title (sprintf "Eggs Unlimited (release branch ~a)" *major-version*))
 	(eggs (gather-egg-information dir)))
     (write-shtml-as-html
      `(html
@@ -67,6 +116,7 @@
 
 (define (prelude title)
   `((h1 ,title)
+    (p (b "Last updated: " ,(seconds->string (current-seconds))))
     (p "A library of extensions for the Chicken Scheme system.")
     (h3 "Installation")
     (p "Just enter")
@@ -102,20 +152,24 @@
   (append-map
    (match-lambda
      ((cat catname)
-      (d "category: ~a" catname)
-      `((h3 ,catname)
-	(table
-	 (tr (th "Name") (th "Description") (th "License") (th "author") (th "maintainer") (th "version"))
-	 ,@(append-map
-	    make-egg-entry
-	    (sort
-	     (filter (lambda (info) 
-		       (and (eq? cat (cadr (or (assq 'category (cdr info))
-					       '(#f uncategorized))))
-			    (not (assq 'hidden (cdr info)))))
-		     eggs) 
-	     (lambda (e1 e2)
-	       (string<? (symbol->string (car e1)) (symbol->string (car e2))))))))))
+      (let ((eggs (append-map
+		   make-egg-entry
+		   (sort
+		    (filter (lambda (info) 
+			      (and (eq? cat (cadr (or (assq 'category (cdr info))
+						      '(#f uncategorized))))
+				   (not (assq 'hidden (cdr info)))))
+			    eggs) 
+		    (lambda (e1 e2)
+		      (string<? (symbol->string (car e1)) (symbol->string (car e2))))))))
+	(if (null? eggs)
+	    '()
+	    (begin
+	      (d "category: ~a" catname)
+	      `((h3 ,catname)
+		(table
+		 (tr (th "Name") (th "Description") (th "License") (th "author") (th "maintainer") (th "version"))
+		 ,@eggs)))))))
    +categories+))
 
 (define (make-egg-entry egg)
@@ -133,9 +187,20 @@
      `((tr (td ,(symbol->string (car egg)))
 	   (td ,(prop 'synopsis "unknown" string?))
 	   (td ,(prop 'license "unknown" name?))
-	   (td ,(prop 'author "unknown" name?))
-	   (td ,(prop 'maintainer "" name?))
+	   (td ,(linkify-names (prop 'author "unknown" name?)))
+	   (td ,(linkify-names (prop 'maintainer "" name?)))
 	   (td ,(prop 'version "" version?)))))))
+
+(define (linkify-names str)
+  ;; silly
+  (html->shtml
+   (open-input-string
+    (irregex-replace/all
+     +link-regexp+ 
+     str
+     (lambda (m)
+       (let ((name (irregex-match-substring m 1)))
+	 (string-append "<a href=\"http://chicken.wiki.br/" name "\">" name "</a>")))))))
 
 (define name?
   (disjoin string? symbol?))
