ba4ce97654e7ff7fcdf2fc4a39c08cde07db6b57
Felix Winkelmann <bunny351@gmail.com>
2009-03-18 13:26:13 +0000

    optional bootstrapping for test-dist
    git-svn-id: https://galinha.ucpel.tche.br/svn/chicken-eggs/chicken/trunk@13814 fca3e652-9b03-0410-8d7b-ac86a6ce46c4

--- scripts/test-dist.sh
+++ scripts/test-dist.sh
@@ -1,40 +1,52 @@
 #!/bin/sh
 ### test-dist.sh - test distribution tarball
 #
-# usage: test-dist.sh PLATFORM TARBALL
+# usage: test-dist.sh [-bootstrap] PLATFORM TARBALL
 
 set -e
 set -x
 
 pwdopts=
+bootstrap=
+
+if test "$1" == "-bootstrap"; then
+    bootstrap=1
+    shift
+fi
 
 if test $# \!= 2; then
-    echo "usage: test-dist.sh PLATFORM TARBALL"
+    echo "usage: test-dist.sh [-bootstrap] PLATFORM TARBALL"
     exit 1
 fi
 
 platform="$1"
 tarball="$2"
 
+# use gmake, if available
+if test -z `which gmake`; then
+    makeprg=make
+fi
+
+# need Windows-style drive letter on mingw/msys
+if test -n "$MSYSTEM"; then
+    pwdopts=-W
+fi
+
+# bootstrap, if desired
+prefix=`pwd $pwdopts`/tmp-test-dist
+
+if test -n "$bootstrap"; then
+    $makeprg PLATFORM=$platform PREFIX=`pwd $pwdopts` DEBUGBUILD=1 bootstrap
+    $makeprg PLATFORM=$platform PREFIX=`pwd $pwdopts` DEBUGBUILD=1 CHICKEN=./chicken-boot confclean all install
+fi
+
+# prepare testing directory
 if test -d tmp-test-dist; then
     rm -fr tmp-test-dist/*
 fi
 
 mkdir -p tmp-test-dist
 cp "$tarball" tmp-test-dist
-
-# need Windows-style drive letter on mingw/msys
-
-if test -n "$MSYSTEM"; then
-    pwdopts=-W
-fi
-
-prefix=`pwd $pwdopts`/tmp-test-dist
-
-# use gmake, if available
-if test -z `which gmake`; then
-    makeprg=make
-fi
 
 # unpack and enter
 cd tmp-test-dist
